994a65aac1136e882937a5bd4e9ecfd75607533f
wahyu.esya17 <wahyu.esya17@gmail.com>
2024-06-21 01:10:43 +0700

    feat:tugas hari-9

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Content from "@/components/content";
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response : ", res))
+      .catch((err) => console.log("error : ", err));
+  }, []);
+
   return (
     <>
       <Layout metaTitle="Home">
